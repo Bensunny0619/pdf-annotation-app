123ec8321b03b6a3def1de02f0fe9dad099fb0b6
Gbenga Emmanuel <98877287+Bensunny0619@users.noreply.github.com>
2025-05-12 13:56:11 +0100

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: resolve(__dirname, 'node_modules/canvas-browserify'),
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
